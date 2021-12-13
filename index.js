c86495bd5606a126ea74599f53c8715cd4e94901
rafaelhecr <rafael@thinkingcapital.org>
2021-12-13 13:33:02 -0600

    clean code & bugs fixed

--- index.js
+++ index.js
@@ -1,108 +1,143 @@
+const dotenv = require('dotenv').config()
 const axios = require('axios');
 const express = require('express');
-// const multer = require('multer');
-// const upload = multer({dest: 'sat_files/'})
 const fileUpload = require('express-fileupload');
 const FormData = require('form-data');
-const {Base64} = require('js-base64');
+const { Base64 } = require('js-base64');
 const fs = require('fs');
 let data = new FormData();
-
-
+//Create express server & middleware for upload files
 const app = express();
 app.use(fileUpload());
 
+//Obtain urls by environtment
+let urlSign = process.env.ENVIRONTMENT === 'pro' ? process.env.SIGN_FUNCTION_PRO : process.env.SIGN_FUNCTION_DEV;
+let urlConvert = process.env.ENVIRONTMENT === 'pro' ? process.env.CONVERT_FUNCTION_PRO : process.env.CONVERT_FUNCTION_DEV;
 
+/** Function check files @function 
+ * @param {array} array_files - Array of files form fileUpload
+ * @return Object of files of throw error if any file is missing
+ */
+function checkFiles(array_files) {
+    let errors = [];
+    if (!array_files.json)
+        errors.push('JSON invoice file is required');
+    if (!array_files.certificate)
+        errors.push('Certificate file is required');
+    if (!array_files.privatekey)
+        errors.push('Private Key file is required');
 
-app.get('/', (req, res)=> {
-    res.status(200).send('Server wiorking');
-});
-
-app.post('/', (req, res)=> {
-    if(req.headers.authorization){
-        let token = req.headers.authorization;
-        //After token is recibed
-        let json_inv = req.files.json;
-        let certificate = req.files.certificate;
-        let privatekey = req.files.privatekey;
-        let passphrase = req.body.passphrase;
-
-        data.append('json', json_inv.data);
-        data.append('certificate', certificate.data);
-        data.append('privatekey', privatekey.data);
-        data.append('passphrase', passphrase);
-        
-        const configRequest = {
-            method: 'post',
-            url: 'http://54.176.12.50/build-cfdi-from-json',
-            headers: { 
-              'Authorization': token, 
-              ...data.getHeaders()
-            },
-            data : data
-        }
-
-        axios(configRequest)
-            .then(response => {
-                let resp = response.data;
-                let uuid = resp.uuid;
-                let xmlGenerated = resp.xml;
-                
-                // console.log("#### resp:", xmlGenerated)
-                let dataToConvertPDF = JSON.stringify({"external": xmlGenerated});
-
-                let configConverter = {
-                    method: 'post',
-                    url: 'https://8f0v07bd48.execute-api.us-west-1.amazonaws.com',
-                    headers: { 
-                        'Content-Type': 'application/json'
-                    },
-                    data : dataToConvertPDF
-                };
-                // console.log('#### configConverted', configConverter);
-
-                axios(configConverter)
-                    .then((resp2) => {
-                        let pdfb64 = resp2.data.fileContent;
-                        // console.log(pdfb64);
-                        // console.log(pdfb64)
-                        let bin =  Base64.atob(pdfb64);
-                        fs.writeFile('invoce_binary.pdf', bin, 'binary', error => {
-                            if(error)
-                                console.log(error);
-                            else{
-                                let invoicePdf = fs.createReadStream('invoce_binary.pdf');
-                                let statInvoicePdf = fs.statSync('invoce_binary.pdf');
-                                res.setHeader('Content-Length', statInvoicePdf.size);
-                                res.setHeader('Content-Type', 'application/pdf');
-                                res.setHeader('Content-Disposition', 'attachment; filename=invoce_binary.pdf');
-                                invoicePdf.pipe(res);
-                                // res.contentType("applicatoion/pdf");
-                                // res.send(invoicePdf)
-                            }
-                        })
-                        // res.status(200).send(JSON.stringify({result: pdfb64}))
-                    })
-                    .catch(function (error) {
-                        console.log('error 2', error);
-                    });
-
-                // res.status(200).send('funciono')
-            })
-            .catch(error => {
-                console.log('#### error', error);
-            })
-        //Show contents of req
-        // console.log('#### req post:', req.files);
-
-        // console.log('#### bearer token',req.headers.authorization);
-        // res.status(200).send('Server working');
+    if (errors.length === 0) {
+        let { json, certificate, privatekey } = array_files;
+        return { json, certificate, privatekey }
     } else {
-        res.status(401).json({error: 'No authorized'})
+        throw new Error(JSON.stringify(errors));
     }
-    
+}
+
+/** Server POST in path / for recived and executes setences, calling two functions , sing function to sing in SAT the XML and convert funtion to convert a XML to PDF base 64;
+ * The b64 string is converted to binary PDF File
+ * @param {any} req - req contains objects with params sended by the client: files and strings.
+ * @param {any} res - res cotaints functions to response the client.
+ */
+
+app.post('/', (req, res) => {
+    if (req.headers.authorization) {
+        try {
+            //Save correct token
+            let token = req.headers.authorization;
+
+            //Check if files exist
+            let { json, certificate, privatekey } = checkFiles(req.files);
+            //Check if passphrase exist
+            let passphrase = req?.body?.passphrase;
+            if (!Boolean(passphrase)) {
+                throw new Error('passphrase required');
+            };
+            //Generate and append files in new Form
+            data.append('json', json.data);
+            data.append('certificate', certificate.data);
+            data.append('privatekey', privatekey.data);
+            data.append('passphrase', passphrase);
+
+            //Object for call sign function
+            const configRequest = {
+                method: 'post',
+                url: `${urlSign}`,
+                headers: {
+                    'Authorization': token,
+                    ...data.getHeaders()
+                },
+                data: data
+            }
+            
+            //Call to sign function
+            axios(configRequest)
+                .then(response => {
+                    //call to sigmn functions success
+                    let resp = response.data;
+                    let xmlGenerated = resp.xml;
+
+                    let dataToConvertPDF = JSON.stringify({ "external": xmlGenerated });
+
+                    let configConverter = {
+                        method: 'post',
+                        url: `${urlConvert}`,
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        data: dataToConvertPDF
+                    };
+
+                    //Call to convert function
+                    axios(configConverter)
+                        .then((resp2) => {
+                            //Call to convert function success
+                            let pdfb64 = resp2.data.fileContent;
+                            let bin = Base64.atob(pdfb64);
+                            fs.writeFile('invoice_binary.pdf', bin, 'binary', error => {
+                                if (error)
+                                    console.log(error);
+                                else {
+                                    let invoicePdf = fs.createReadStream('invoice_binary.pdf');
+                                    let statInvoicePdf = fs.statSync('invoice_binary.pdf');
+                                    res.setHeader('Content-Length', statInvoicePdf.size);
+                                    res.setHeader('Content-Type', 'application/pdf');
+                                    res.setHeader('Content-Disposition', 'attachment; filename=invoice_binary.pdf');
+                                    invoicePdf.pipe(res);
+                                }
+                            })
+                        })
+                        .catch(function (error) {
+                            //Call to convert funcion fails
+                            const errorR = `Error call to convert funcion: ${error}`
+                            console.log(errorR);
+                            res.status(400).send(errorR);
+                        });
+                })
+                .catch(error => {
+                    //Call to sign function fails.
+                    const errorR = `Error call to sign funcion: ${error}`
+                    console.log(errorR);
+                    res.status(400).send(String(errorR));
+                })
+
+        } catch (error) {
+            //Sends error if any fails
+            res.status(400).send(String(error));
+        }
+    } else {
+        //Sends error if token is bad
+        res.status(401).json({ error: 'No authorized' })
+    }
+
 });
 
 app.listen(3000, () => {
     console.log(`Server running in por 3000`);
 })
+
+//Response of server if is calling by browser
+app.get('/', (req, res) => {
+    res.status(200).send('Server wiorking');
+});
